1da65174402742b977b4ee1748fc3df807917a59
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-02-13 20:15:29 +0100

    add logout

--- join/urls.py
+++ join/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
-from login.views import LoginView
+from login.views import LoginView, Logout
 from tasks.views import TaskDetailsViewSet, TaskViewSet
 from users.views import UserDetailsViewSet, UsersViewSet
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', lambda request: redirect('tasks/', permanent=True)),
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view()),
+    path('logout/', Logout.as_view()),
     path('tasks/', TaskViewSet.as_view()),
     path('tasks/<int:pk>/', TaskDetailsViewSet.as_view()),
     path('users/', UsersViewSet.as_view()),
--- login/views.py
+++ login/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import logout
+from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 class LoginView(ObtainAuthToken):
@@ -16,3 +19,8 @@ class LoginView(ObtainAuthToken):
             'user_id': user.pk,
             'email': user.email
         })
+
+class Logout(APIView):
+    def get(self, request, format=None):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
